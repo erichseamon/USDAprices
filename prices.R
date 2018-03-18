a21bb634d17fbd8e4ff97abb09d458b1b68175d9
Erich Seamon <erichs@uidaho.edu>
2018-03-17 23:57:02 -0700

    initial commit

---
+++ prices.R
@@ -0,0 +1,3 @@
+
+barley <- read.csv("/dmine/data/USDAprices/barleyprices_1988_2017.csv", header=TRUE, strip.white =TRUE)
+wheat <- read.csv("/dmine/data/USDAprices/wheatprices_1988_2017.csv", header=TRUE, strip.white =TRUE)
